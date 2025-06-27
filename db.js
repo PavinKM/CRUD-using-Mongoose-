23d10b6f08a08c0b2ef0daca75fcdc2ba2881b54
PavinKM <pavinyagava@gmail.com>
2025-06-27 22:25:33 +0530

    mongobd atlas

--- db.js
+++ db.js
@@ -15,13 +15,33 @@ async function getDatabase() {
 
     // return database
 
-    mongoose.connect('mongodb://127.0.0.1:27017/library')
+    mongoose.connect('mongodb+srv://pavinkm:'+encodeURIComponent('@Test20')+'@cluster0.vqvjq0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    // mongodb://127.0.0.1:27017/library 
+    // mongodb+srv://pavinkm:<db_password>@cluster0.vqvjq0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
     .then(()=> {
         console.log('Database Connected!')
     })
-    .catch(()=>{
-        console.log('Database Connection Error!')
+    .catch((err)=>{
+        console.log('Database Connection Error!', err)
     })
 }
 module.exports = { getDatabase }
 // module.exports = { getDatabase, ObjectID }
+
+
+
+// const mongoose = require('mongoose')
+// let database
+// async function getDatabase() {
+//  mongoose.connect('mongodb+srv://pavinkm:'+encodeURIComponent('@Test20')+'@cluster0.vqvjq0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+// .then(()=> {
+//         console.log('Database Connected!')
+//     })
+//     .catch((err)=>{
+//         console.log('Database Connection Error!', err)
+//     })
+// }
+// module.exports = { getDatabase }
+
+
+
